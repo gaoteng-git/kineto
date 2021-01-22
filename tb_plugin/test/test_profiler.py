bcc47f0ac47cfc5d7bbc91421d4ca383f1a4e1db
Teng Gao <tegao@microsoft.com>
2021-01-22 21:34:26 +0800

    add more tracing files

--- tb_plugin/test/test_profiler.py
+++ tb_plugin/test/test_profiler.py
@@ -1,52 +1,59 @@
 import jsonpickle
 import jsonpickle.ext.pandas as jsonpickle_pd
 import unittest
-import math
+import os
 
 from tensorboard_plugin_torch_profiler.profiler.loader import RunLoader
+import tensorboard_plugin_torch_profiler.consts as consts
 
-RESNET50 = "./data/Resnet50.json"
+TRACING_DIR = "./data/tracing"
+PLUGIN_PROFILE_DIR = "./data/plugin_profile"
+PLUGIN_PROFILE_POSTFIX = ".profile.json"
 
 
 class TestProfiler(unittest.TestCase):
-    def compare_list(self, list1, list2):
-        self.assertEqual(len(list1), len(list2))
-        for i in range(len(list1)):
-            self.assertTrue(math.isclose(list1[i], list2[i]))
-
     def test_run(self):
-        run_loader = RunLoader("test_profiler", "./data")
-        run = run_loader.load()
-        with open(RESNET50, "r") as file:
-            run_expected = jsonpickle.decode(file.read())
+        def compare_runs(run, run_expected):
+            self.assertEqual(len(run.profiles), len(run_expected.profiles))
+            for key in run.profiles.keys():
+                self.assertTrue(key in run.profiles)
+                prof = run.profiles[key]
+                prof_expected = run_expected.profiles[key]
 
-        self.assertEqual(len(run.profiles), len(run_expected.profiles))
-        for key in run.profiles.keys():
-            self.assertTrue(key in run.profiles)
+                overview_rows = prof.overview["steps"]["rows"]
+                overview_rows_expected = prof_expected.overview["steps"]["rows"]
+                self.assertEqual(overview_rows, overview_rows_expected)
 
-            overview_rows = run.profiles[key].overview["steps"]["rows"]
-            overview_rows_expected = run_expected.profiles[key].overview["steps"]["rows"]
-            self.assertEqual(len(overview_rows), len(overview_rows_expected))
-            for i in range(len(overview_rows)):
-                if isinstance(overview_rows[i], float):
-                    self.assertTrue(isinstance(overview_rows_expected[i], float))
-                    self.assertTrue(math.isclose(overview_rows[i], overview_rows_expected[i]))
+                overview_avgs = prof.overview["performance"][0]["children"]
+                overview_avgs_expected = prof_expected.overview["performance"][0]["children"]
+                self.assertEqual(overview_avgs, overview_avgs_expected)
 
-            overview_avgs = run.profiles[key].overview["performance"][0]["children"]
-            overview_avgs_expected = run_expected.profiles[key].overview["performance"][0]["children"]
-            self.assertEqual(len(overview_avgs), len(overview_avgs_expected))
-            for i in range(len(overview_avgs)):
-                self.assertEqual(overview_avgs[i]["name"], overview_avgs_expected[i]["name"])
-                self.assertTrue(math.isclose(overview_avgs[i]["value"], overview_avgs_expected[i]["value"]))
+                op_by_name = prof.operation_table_by_name
+                op_by_name_expected = prof_expected.operation_table_by_name
+                # No float number, so assertEqual works.
+                self.assertEqual(op_by_name, op_by_name_expected)
 
-            op_by_name = run.profiles[key].operation_table_by_name["data"]["rows"]
-            op_by_name_expected = run_expected.profiles[key].operation_table_by_name["data"]["rows"]
-            self.assertEqual(op_by_name, op_by_name_expected)
+                op_by_name_input = prof.operation_table_by_name_input
+                op_by_name_input_expected = prof_expected.operation_table_by_name_input
+                # No float number, so assertEqual works.
+                self.assertEqual(op_by_name_input, op_by_name_input_expected)
 
-            op_by_name_input = run.profiles[key].operation_table_by_name_input["data"]["rows"]
-            op_by_name_input_expected = run_expected.profiles[key].operation_table_by_name_input["data"]["rows"]
-            self.assertEqual(op_by_name_input, op_by_name_input_expected)
+                if consts.KERNEL_VIEW in prof_expected.views:
+                    kernel_op_table = prof.kernel_op_table
+                    kernel_op_table_expected = prof_expected.kernel_op_table
+                    self.assertEqual(kernel_op_table, kernel_op_table_expected)
 
+                    kernel_table = prof.kernel_table
+                    kernel_table_expected = prof_expected.kernel_table
+                    self.assertEqual(kernel_table, kernel_table_expected)
+
+        for run_dir in os.listdir(TRACING_DIR):
+            run_loader = RunLoader(run_dir, os.path.join(TRACING_DIR, run_dir))
+            run = run_loader.load()
+            profile_path = os.path.join(PLUGIN_PROFILE_DIR, "{}.{}".format(run_dir, PLUGIN_PROFILE_POSTFIX))
+            with open(profile_path, "r") as file:
+                run_expected = jsonpickle.decode(file.read())
+                compare_runs(run, run_expected)
 
 if __name__ == '__main__':
     jsonpickle_pd.register_handlers()
