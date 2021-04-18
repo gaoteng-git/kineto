c3279077db8ecc690fea6b02dc5f6e9442ef5348
Teng Gao <tegao@microsoft.com>
2021-04-18 23:37:24 +0800

    update

--- tb_plugin/torch_tb_profiler/profiler/kernel_parser.py
+++ tb_plugin/torch_tb_profiler/profiler/kernel_parser.py
@@ -10,11 +10,11 @@ class KernelParser:
         self.kernel_stat = None
 
     def parse_events(self, events):
-        self.dump_occupancy_sm_efficiency(events)
         events_dict = []
         for event in events:
             events_dict.append(vars(event))
         events = events_dict
+        self.dump_occupancy_sm_efficiency(events)
         events = pd.DataFrame(events)
         events = events.astype({"type": "category", "category": "category", "name": "string"}, copy=False)
         kernels = events[events["category"] == "Kernel"]
@@ -26,18 +26,18 @@ class KernelParser:
         file = open("occupancy_sm_efficiency.json", "w")
         last_kernel_ts = 0
         for event in events:
-            if event["category"] == "Kernel":
+            if (event["category"] == "Kernel") and ("ts" in event) and ("duration" in event):
                 begin_ts = event["ts"]
-                end_ts = event["ts"] + event["dur"]
+                end_ts = event["ts"] + event["duration"]
                 if begin_ts >= last_kernel_ts:
                     last_kernel_ts = end_ts
-                    occupancy_begin = '{"ph": "C", "name": "Occupancy", "pid": 0, "tid": "stream 7", "ts": {}, "args": {"Occupancy": {}}}'.format(
-                        begin_ts, event["occupancy"])
-                    occupancy_end = '{"ph": "C", "name": "Occupancy", "pid": 0, "tid": "stream 7", "ts": {}, "args": {"Occupancy": {}}}'.format(
+                    occupancy_begin = '{{"ph": "C", "name": "Occupancy", "pid": 0, "tid": "stream 7", "ts": {}, "args": {{"Occupancy": {}}}}},'.format(
+                        begin_ts, event["args"]["occupancy"])
+                    occupancy_end = '{{"ph": "C", "name": "Occupancy", "pid": 0, "tid": "stream 7", "ts": {}, "args": {{"Occupancy": {}}}}},'.format(
                         end_ts, 0)
-                    sm_utilization_begin = '{"ph": "C", "name": "SM_utilization", "pid": 0, "tid": "stream 7", "ts": {}, "args": {"SM_utilization": {}}}'.format(
-                        begin_ts, event["sm_efficiency"])
-                    sm_utilization_end = '{"ph": "C", "name": "SM_utilization", "pid": 0, "tid": "stream 7", "ts": {}, "args": {"SM_utilization": {}}}'.format(
+                    sm_utilization_begin = '{{"ph": "C", "name": "SM_utilization", "pid": 0, "tid": "stream 7", "ts": {}, "args": {{"SM_utilization": {}}}}},'.format(
+                        begin_ts, event["args"]["sm_efficiency"])
+                    sm_utilization_end = '{{"ph": "C", "name": "SM_utilization", "pid": 0, "tid": "stream 7", "ts": {}, "args": {{"SM_utilization": {}}}}},'.format(
                         end_ts, 0)
                     file.write(occupancy_begin + "\n")
                     file.write(occupancy_end + "\n")
